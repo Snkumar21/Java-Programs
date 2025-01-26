b913c9a6aef68f8557a7c1139a96cbf69a2f232f
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-01-26 14:02:02 +0530

    Update divideFinally.java

--- divideFinally.java
+++ divideFinally.java
@@ -1,3 +1,4 @@
+// Finally keyword used in Java Program
 import java.util.Scanner;
 
 public class divideFinally
@@ -6,13 +7,14 @@ public class divideFinally
 	{
 		Scanner scan = new Scanner(System.in);
 
+		//User defined numbers
         	System.out.println("Enter the first number : ");
 		int num1 = scan.nextInt();
 
 		System.out.println("Enter the second number : ");
 		int num2 = scan.nextInt();
 
-		int result = num1/num2;
+		int result = num1 / num2; // Condition
 
 		if(result != 0)
 		{
