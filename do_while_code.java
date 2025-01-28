342c034ed42265efc0057ec8d5b177cbd3c93d15
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-01-28 10:22:44 +0530

    Update do_while_code.java

--- do_while_code.java
+++ do_while_code.java
@@ -1,15 +1,11 @@
-class do_while_code{
-	public static void main(String Args[]){
-
+//This is do-while program using Java
+class do_while_code {
+	public static void main(String Args[]) {
 		int num=5, i=1;
 
-		do
-		{
-	
+		do {
 			System.out.println(i);
 			i++;
-
 		}while(i<=num);
-
 	}
 }
