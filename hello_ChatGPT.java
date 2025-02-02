8b37001f3fef9d379554e81272a70610571ae2ad
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-02-02 11:22:25 +0530

    Update and rename helloworld.java to hello_ChatGPT.java

--- hello_ChatGPT.java
+++ hello_ChatGPT.java
@@ -1,4 +1,5 @@
-class helloworld {
+//This is a Hello World program in pre-defiened in java.
+class hello_ChatGPT {
 	public static void main(String[] Args) {
 		System.out.println("Hello ChatGPT...!");
 	}
