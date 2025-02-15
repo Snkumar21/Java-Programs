ff0b817b7245fdf6229580f8c91c7ff38a8ea3c9
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-02-15 16:34:00 +0530

    Add files via upload

---
+++ oddeven_forloop.java
@@ -0,0 +1,16 @@
+class oddeven_forloop{
+	public static void main(String Args[]){
+	
+		for( int i=1; i<=100; i++)
+		{
+			if(i%2==0)
+			{
+				System.out.println("The number is even " + i);
+			}
+			else	
+			{
+				System.out.println("The number is odd " + i);
+			}
+		}
+	}
+}
