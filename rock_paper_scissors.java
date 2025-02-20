9ebd38fba879b6353240dbeb78a6500300c7c7d9
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-02-20 11:59:55 +0530

    Update and rename RockPaperScissors.java to rock_paper_scissors.java

--- rock_paper_scissors.java
+++ rock_paper_scissors.java
@@ -1,7 +1,9 @@
+// This is rock-paper-scissor game using java.
+
 import java.util.Scanner;
 import java.util.Random;
 
-public class RockPaperScissors {
+public class rock_paper_scissors {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
