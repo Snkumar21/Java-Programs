f245a894271ffe62f356a7374eca0d7f97c78afe
Nitish Kumar Singh <142076381+Snkumar21@users.noreply.github.com>
2025-02-23 00:20:40 +0530

    Update sorting.java

--- sorting.java
+++ sorting.java
@@ -1,27 +1,27 @@
+// Sorting code in java in ascending order.
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
 class sorting {
 
-    	@SuppressWarnings("resource")
-		public static void main(String[] args) {
-        	Scanner scan = new Scanner(System.in);
+	public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
-        	System.out.println("Enter the number of elements:");
-        	int n = scan.nextInt();
+        System.out.println("Enter the number of elements:");
+        int n = scan.nextInt();
 
-        	Integer[] arr = new Integer[n];
+        Integer[] arr = new Integer[n];
 
-        	System.out.println("Enter the numbers:");
-        	for (int i = 0; i < n; i++) {
-        	    	arr[i] = scan.nextInt();
-        	}
+        System.out.println("Enter the numbers:");
+        for (int i = 0; i < n; i++) {
+	 	arr[i] = scan.nextInt();
+        }
 
-	        Arrays.sort(arr);
-        	System.out.println("Ascending Order: " + Arrays.toString(arr));
+	Arrays.sort(arr);
+        System.out.println("Ascending Order: " + Arrays.toString(arr));
 
-        	Arrays.sort(arr, Collections.reverseOrder());
-        	System.out.println("Descending Order: " + Arrays.toString(arr));
-    	}
+        Arrays.sort(arr, Collections.reverseOrder());
+        System.out.println("Descending Order: " + Arrays.toString(arr));
+    }
 }
