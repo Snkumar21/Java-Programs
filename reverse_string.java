d2c4bb04e85397ce8c2847b0aa5e52faf99741d7
Nitish <snkumar6926@gmail.com>
2025-04-25 11:27:40 +0530

    new changes added

--- reverse_string.java
+++ reverse_string.java
@@ -1,9 +1,8 @@
-<<<<<<< HEAD
+// This is a program for reverse a string using user defined in java program.
 import java.util.Scanner;
 
-class reverse_string {
+public class reverse_string {
     public static void main(String[] args) {
-
         try (Scanner scanner = new Scanner(System.in)) {
             System.out.print("Enter a string: ");
             String inputString = scanner.nextLine();
@@ -11,24 +10,5 @@ class reverse_string {
             String reversed = new StringBuilder(inputString).reverse().toString();
             System.out.println("Reversed string: " + reversed);
         }
-        
     }
 }
-=======
-// This is a program for reverse a string using user defined in java program.
-import java.util.Scanner;
-
-public class reverse_string
- {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String inputString = scanner.nextLine();
-
-        String reversed = new StringBuilder(inputString).reverse().toString();
-        System.out.println("Reversed string: " + reversed);
-
-        scanner.close();
-    }
-}
->>>>>>> 3b91145e87f179c688b01060e6f387c287eacd71
--- right_angle_triangle_star.java
+++ right_angle_triangle_star.java
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-public class right_angle_triangle_star {
-    public static void main(String Args[]) {
-        int rows = 5;
-
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*  ");
-            }
-            System.out.println();
-        }
-    }    
-}
-=======
 // This is pattern program of * star in right angle triangle format.
 public class right_angle_triangle_star {
     public static void main(String Args[]) {
@@ -25,4 +11,3 @@ public class right_angle_triangle_star {
         }
     }    
 }
->>>>>>> 3b91145e87f179c688b01060e6f387c287eacd71
