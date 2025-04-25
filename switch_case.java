18376df4a0fa7b90eebafc09c90c6de0a39b8510
Nitish <snkumar6926@gmail.com>
2025-04-25 11:40:52 +0530

    new changes added

--- switch_case.java
+++ switch_case.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import java.util.Scanner;
 public class switch_case {
     public static void main(String[] args) {
@@ -25,32 +24,4 @@ public class switch_case {
             }
         }
     }
-=======
-import java.util.Scanner;
-public class switch_case {
-    public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            System.out.println("Enter a number (choose from 1-7) :");
-            int choice = sc.nextInt();
-
-            switch (choice) {
-                case 1 -> System.out.println("Monday");
-
-                case 2 -> System.out.println("Tuesday");
-
-                case 3 -> System.out.println("Wednesday");
-
-                case 4 -> System.out.println("Thursday");
-
-                case 5 -> System.out.println("Friday");
-
-                case 6 -> System.out.println("Saturday");
-
-                case 7 -> System.out.println("Sunday");
-
-                default -> System.out.println("Invalid choice");
-            }
-        }
-    }
->>>>>>> 3b91145e87f179c688b01060e6f387c287eacd71
 }
