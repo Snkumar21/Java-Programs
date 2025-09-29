5b8c9dd4dc2174e254aeed06a4b3d98de2627c85
Nitish <snkumar6926@gmail.com>
2025-09-29 14:31:11 +0530

    New code added

--- stackReverseOrder.java
+++ stackReverseOrder.java
@@ -2,23 +2,23 @@ import java.util.*;
 
 public class stackReverseOrder {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            Stack<Integer> stack = new Stack<>();
 
-        Stack<Integer> stack = new Stack<>();
+            // Input elements
+            System.out.println("Enter number of elements in stack:");
+            int n = sc.nextInt();
 
-        // Input elements
-        System.out.println("Enter number of elements in stack:");
-        int n = sc.nextInt();
+            System.out.println("Enter " + n + " elements:");
+            for (int i = 0; i < n; i++) {
+                stack.push(sc.nextInt());
+            }
 
-        System.out.println("Enter " + n + " elements:");
-        for (int i = 0; i < n; i++) {
-            stack.push(sc.nextInt());
+            System.out.println("Original Stack (Top to Bottom): " + stack);
+
+            System.out.print("Stack in Reverse Order (Bottom to Top): ");
+            printReverse(stack);
         }
-
-        System.out.println("Original Stack (Top to Bottom): " + stack);
-
-        System.out.print("Stack in Reverse Order (Bottom to Top): ");
-        printReverse(stack);
     }
 
     // Method to print stack in reverse order (without modifying it)
